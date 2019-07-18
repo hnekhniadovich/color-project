3af88da42d6f852f5764308480a6d24c8cb20b58
hnekhniadovich <nekhanya@gmail.com>
2019-07-17 20:52:20 -0400

    refactor navbar styles and remove css

--- src/Palette.js
+++ src/Palette.js
@@ -5,8 +5,6 @@ import PaletteFooter from './PaletteFooter';
 import styles from './styles/PaletteStyles';
 import { withStyles } from '@material-ui/styles';
 
-import './Palette.css';
-
 class Palette extends Component {
     constructor(props) {
         super(props);
